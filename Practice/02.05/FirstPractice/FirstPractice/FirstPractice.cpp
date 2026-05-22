4bebed85ec05c23d1f4bd2e6b8f5887f730bf518
sagjar22 <foffikfoffik@gmail.com>
2026-05-22 17:34:36 +0300

    All practice tasks 1-8

--- Practice/02.05/FirstPractice/FirstPractice/FirstPractice.cpp
+++ Practice/02.05/FirstPractice/FirstPractice/FirstPractice.cpp
@@ -1,20 +1,184 @@
-// FirstPractice.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Task 1
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << "The number is even" << '\n';
+    else
+        cout << "The number is odd" << '\n';
+
+    cout << '\n';
+
+    // Task 2
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a < b)
+        cout << "Smaller number: " << a << '\n';
+    else
+        cout << "Smaller number: " << b << '\n';
+
+    cout << '\n';
+
+    // Task 3
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number > 0)
+        cout << "Positive number" << '\n';
+    else if (number < 0)
+        cout << "Negative number" << '\n';
+    else
+        cout << "The number is zero" << '\n';
+
+    cout << '\n';
+
+    // Task 4
+    int x, y;
+
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    if (x == y)
+        cout << "Numbers are equal" << '\n';
+    else if (x < y)
+        cout << x << " " << y << '\n';
+    else
+        cout << y << " " << x << '\n';
+
+    cout << '\n';
+
+    // Task 5
+    int mark1, mark2, mark3, mark4, mark5;
+
+    cout << "Enter 5 marks: ";
+    cin >> mark1 >> mark2 >> mark3 >> mark4 >> mark5;
+
+    double average = (mark1 + mark2 + mark3 + mark4 + mark5) / 5.0;
+
+    if (average >= 4)
+        cout << "Student is admitted to the exam" << '\n';
+    else
+        cout << "Student is not admitted to the exam" << '\n';
+
+    cout << '\n';
+
+    // Task 6
+    int value;
+
+    cout << "Enter a number: ";
+    cin >> value;
+
+    if (value % 2 == 0)
+        value *= 3;
+    else
+        value /= 2;
+
+    cout << "Result: " << value << '\n';
+
+    cout << '\n';
+
+    // Task 7
+    double num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operation (+ - * /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+        cout << "Result: " << num1 + num2 << '\n';
+        break;
+
+    case '-':
+        cout << "Result: " << num1 - num2 << '\n';
+        break;
+
+    case '*':
+        cout << "Result: " << num1 * num2 << '\n';
+        break;
+
+    case '/':
+        if (num2 != 0)
+            cout << "Result: " << num1 / num2 << '\n';
+        else
+            cout << "Division by zero is impossible" << '\n';
+        break;
+
+    default:
+        cout << "Invalid operation" << '\n';
+    }
+
+    cout << '\n';
+    // Task 8
+    double base;
+    int power;
+    double result = 1;
+
+    cout << "Enter a number: ";
+    cin >> base;
+
+    cout << "Enter power (0-7): ";
+    cin >> power;
+
+    switch (power)
+    {
+    case 0:
+        result = 1;
+        break;
+
+    case 1:
+        result = base;
+        break;
+
+    case 2:
+        result = base * base;
+        break;
+
+    case 3:
+        result = base * base * base;
+        break;
+
+    case 4:
+        result = base * base * base * base;
+        break;
+
+    case 5:
+        result = base * base * base * base * base;
+        break;
+
+    case 6:
+        result = base * base * base * base * base * base;
+        break;
+
+    case 7:
+        result = base * base * base * base * base * base * base;
+        break;
+
+    default:
+        cout << "Invalid power" << '\n';
+        return 0;
+    }
+
+    cout << "Result: " << result << '\n';
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
