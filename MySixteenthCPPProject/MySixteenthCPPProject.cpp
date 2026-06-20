9c41d676ab446c8fef72cd0aa3dc83cb891c9854
sagjar22 <foffikfoffik@gmail.com>
2026-06-20 13:20:30 +0300

    s

---
+++ MySixteenthCPPProject/MySixteenthCPPProject.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum Weekdays {
+    Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun
+};
+
+struct fieldbits {
+    unsigned char field1 : 1;
+    unsigned char field2 : 3;
+    unsigned char field3 : 4;
+};
+
+
+struct date {
+    int day;
+    int month;
+    int year;
+    Weekdays weekday;
+    char month_name[15];
+};
+
+struct person {
+    char* firstName;
+    char* secondName;
+    char* email;
+    date bithDate;
+};
+
+void printDate(date dateToPrint) {
+    cout << dateToPrint.day << '.'
+        << dateToPrint.month << '.'
+        << dateToPrint.year << " -- "
+        << dateToPrint.weekday << ' '
+        << dateToPrint.month_name;
+}
+
+int main()
+{
+    date today = { 20, 06,2026, Sat, "June" };
+    printDate(today);
+
+    date* datePtr = &today;
+
+    cout << '\n';
+
+    //cout << (*datePtr).day;
+    cout << datePtr->day;
+}
