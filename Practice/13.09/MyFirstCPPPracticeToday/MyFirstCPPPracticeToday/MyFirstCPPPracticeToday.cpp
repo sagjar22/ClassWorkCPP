7516d8f20c2017a55acf25f64bb9b67414e67854
sagjar22 <foffikfoffik@gmail.com>
2026-09-13 17:54:42 +0300

    start do practice 50% complete

--- Practice/13.09/MyFirstCPPPracticeToday/MyFirstCPPPracticeToday/MyFirstCPPPracticeToday.cpp
+++ Practice/13.09/MyFirstCPPPracticeToday/MyFirstCPPPracticeToday/MyFirstCPPPracticeToday.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 
-class Group {
+#define MAX_NAME_LEN 50
 
+class Group {
+private: 
+    char* name = nullptr;
+    int age;
+public: 
+    ~Group() {
+        if (name != nullptr) {
+            delete[] name;
+        }
+    }
+
+    Group() : Group("Unknown", 0) {}
+
+    Group(const char* initialName, int initialAge) :age(initialAge) {
+        setName(initialName);
+    }
+
+    Group(const Group&)
+
+    void setName(const char* newName) {
+        if (name != nullptr) {
+            delete[] name;
+        }
+        if (newName == nullptr) {
+            newName = "Unknown";
+        }
+        name = new
+        char[strlen(newName) + 1, newName];
+        strcpy_s(name, strlen(newName) +1, newName);
+    }
+
+    void setAge(int newAge) {
+        age = newAge;
+    }
+
+    const char* const getName() {
+        return name;
+    }
+    
+    int getAge() {
+        return age;
+    }
 };
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
